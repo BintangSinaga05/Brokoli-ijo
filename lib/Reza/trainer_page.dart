e35a61b28ec0da5e2e2b6661afc4c55847b2ea73
Reza Hermawan <rezahermawan10969@gmail.com>
2023-10-23 00:33:19 +0700

    Update Trainer Page,Membuat Fitur Search pada Page Training

--- lib/Reza/trainer_page.dart
+++ lib/Reza/trainer_page.dart
@@ -1,43 +1,138 @@
 import 'package:flutter/material.dart';
 
-class TrainerPage extends StatelessWidget {
-  TrainerPage({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: TrainerPage(),
+    );
+  }
+}
+
+class TrainerPage extends StatefulWidget {
+  const TrainerPage({Key? key}) : super(key: key);
+
+  @override
+  _TrainerPageState createState() => _TrainerPageState();
+}
+
+class _TrainerPageState extends State<TrainerPage> {
+  String searchText = '';
+  List<TrainerData> searchResults = [];
 
   final List<TrainerData> trainers = [
     TrainerData(
       name: "James Doe",
       gym: "Trainer Gym",
       experience: "8 Years",
-      rating: "95 %",
-      foto: "Trainer1.jpeg",
+      rating: 95,
+      asset: "Trainer1.jpeg",
     ),
     TrainerData(
       name: "Richard",
       gym: "Trainer Gym",
       experience: "10 Years",
-      rating: "90 %",
-      foto: "Trainer2.jpeg",
+      rating: 90,
+      asset: "Trainer2.jpeg",
     ),
     TrainerData(
       name: "Adam Smith",
       gym: "Trainer Gym",
       experience: "20 Years",
-      rating: "100 %",
-      foto: "Trainer3.jpeg",
+      rating: 100,
+      asset: "Trainer3.jpeg",
+    ),
+    TrainerData(
+      name: "John Doe",
+      gym: "Trainer Gym",
+      experience: "12 Years",
+      rating: 98,
+      asset: "Trainer4.jpeg",
+    ),
+    TrainerData(
+      name: "Sarah Johnson",
+      gym: "FitLife",
+      experience: "15 Years",
+      rating: 92,
+      asset: "Trainer5.jpeg",
+    ),
+    TrainerData(
+      name: "Emily Wilson",
+      gym: "PowerFit",
+      experience: "18 Years",
+      rating: 97,
+      asset: "Trainer6.jpeg",
+    ),
+    TrainerData(
+      name: "Michael Brown",
+      gym: "FitnessPro",
+      experience: "7 Years",
+      rating: 89,
+      asset: "Trainer7.jpeg",
+    ),
+    TrainerData(
+      name: "Sophia Davis",
+      gym: "Elite Fitness",
+      experience: "9 Years",
+      rating: 96,
+      asset: "Trainer8.jpeg",
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(color: Color(0xFF18141D)),
-        child: ListView.builder(
-          itemCount: trainers.length,
-          itemBuilder: (context, index) {
-            return TrainerCard(trainerData: trainers[index]);
-          },
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              color: const Color(0xFF18141D),
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  labelText: 'Search',
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                onChanged: (text) {
+                  setState(() {
+                    searchText = text;
+                    searchResults = trainers
+                        .where((trainer) => trainer.name
+                            .toLowerCase()
+                            .contains(text.toLowerCase()))
+                        .toList();
+                  });
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(color: Color(0xFF18141D)),
+              child: ListView.builder(
+                itemCount:
+                    searchText.isEmpty ? trainers.length : searchResults.length,
+                itemBuilder: (context, index) {
+                  final trainer = searchText.isEmpty
+                      ? trainers[index]
+                      : searchResults[index];
+                  return TrainerCard(trainerData: trainer);
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -47,15 +142,15 @@ class TrainerData {
   final String name;
   final String gym;
   final String experience;
-  final String rating;
-  final String foto;
+  final int rating;
+  final String asset;
 
   TrainerData({
     required this.name,
     required this.gym,
     required this.experience,
     required this.rating,
-    required this.foto,
+    required this.asset,
   });
 }
 
@@ -66,13 +161,15 @@ class TrainerCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
       margin: const EdgeInsets.all(20),
       child: Row(
         children: [
           Image.asset(
-            "assets/${trainerData.foto}",
-            width: 150,
+            "assets/${trainerData.asset}",
+            width: screenWidth * 0.4, // Adjust as needed
           ),
           Container(
             margin: const EdgeInsets.only(left: 12),
@@ -81,9 +178,9 @@ class TrainerCard extends StatelessWidget {
               children: [
                 Text(
                   trainerData.name,
-                  style: const TextStyle(
+                  style: TextStyle(
                     color: Colors.white,
-                    fontSize: 27,
+                    fontSize: screenWidth * 0.04, // Adjust as needed
                     fontWeight: FontWeight.w900,
                   ),
                 ),
@@ -105,7 +202,7 @@ class TrainerCard extends StatelessWidget {
                         children: [
                           Image.asset(
                             "assets/trainerlogo.png",
-                            width: 20,
+                            width: screenWidth * 0.05, // Adjust as needed
                           ),
                           const SizedBox(
                             width: 8,
@@ -124,7 +221,7 @@ class TrainerCard extends StatelessWidget {
                       child: Row(
                         children: [
                           const Icon(Icons.thumb_up),
-                          Text(trainerData.rating),
+                          Text('${trainerData.rating.toString()}%'),
                         ],
                       ),
                     ),
@@ -151,10 +248,4 @@ class TrainerCard extends StatelessWidget {
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: TrainerPage(),
-  ));
 }
