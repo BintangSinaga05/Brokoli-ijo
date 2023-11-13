ffb24dc9eba2b8ed837767c610d2a656a5f80118
Frans Yohanes <fransyohanes73@gmail.com>
2023-11-13 20:04:59 +0700

    update auth efficiency code

--- lib/Page/Login&Register_page/auth.dart
+++ lib/Page/Login&Register_page/auth.dart
@@ -1,4 +1,3 @@
-import 'package:basic/Page/Login&Register_page/exception/exception.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -6,40 +5,16 @@ class AuthFirebase {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   Future<String?> signUp(String email, String password) async {
-    try {
-      UserCredential authResult = await _firebaseAuth
-          .createUserWithEmailAndPassword(email: email, password: password);
-      User? user = authResult.user;
-      return user?.uid;
-    } on FirebaseAuthException catch (e) {
-      final ex = Failure.code(e.code);
-      print('FIREBASE AUTH EXCEPTION - ${ex.message}');
-      throw ex;
-    } catch (_) {
-      const ex = Failure();
-      print('EXCEPTION - ${ex.message}');
-      throw ex;
-    }
+    UserCredential authResult = await _firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    User? user = authResult.user;
+    return user?.uid;
   }
 
   Future<String?> login(String email, String password) async {
-    try {
-      UserCredential authResult = await _firebaseAuth
-          .signInWithEmailAndPassword(email: email, password: password);
-      User? user = authResult.user;
-      return user?.uid;
-    } catch (e) {
-      final snackbar = SnackBar(
-        content: Text("$e"),
-        action: SnackBarAction(label: 'OK', onPressed: () {}),
-      );
-      ScaffoldMessenger(child: snackbar);
-      return null;
-    }
-  }
-
-  Future<User?> getUser() async {
-    User? user = _firebaseAuth.currentUser;
-    return user;
+    UserCredential authResult = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    User? user = authResult.user;
+    return user?.uid;
   }
 }
