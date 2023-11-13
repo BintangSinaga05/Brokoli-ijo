173b904982e6b746e438409eabc9d2efb7ece95c
Rizki Pane <rizkipane507@gmail.com>
2023-11-13 20:22:14 +0700

    Update register_page
    Handle error pada register page berupa handle email jika sudah digunakan, format email salah, tidak mengisi teks field email dan password.menggunakan Try catch yang di handle oleh FirebaseAuthExeption

--- lib/Page/Login&Register_page/register_page.dart
+++ lib/Page/Login&Register_page/register_page.dart
@@ -11,7 +11,7 @@ class Register extends StatefulWidget {
   @override
   _RegisterState createState() => _RegisterState();
 }
-
+ 
 class _RegisterState extends State<Register> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
