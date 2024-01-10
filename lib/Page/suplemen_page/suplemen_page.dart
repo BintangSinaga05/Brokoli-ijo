5018dfa9a624a410f624921d486773324658302c
Rizki Pane <rizkipane507@gmail.com>
2024-01-11 00:07:18 +0700

    add icon
    Membuat icon cart pada cart page dan suplemen page

--- lib/Page/suplemen_page/suplemen_page.dart
+++ lib/Page/suplemen_page/suplemen_page.dart
@@ -136,7 +136,7 @@ class _SuplemenPageState extends State<SuplemenPage> {
                           Text("Harga: ${suplemen.hargasuplemen}"),
                         ],
                       ),
-                      trailing: const Text("Add to Cart"),
+                      trailing: ElevatedButton(onPressed: (){}, child: Icon(Icons.shopping_cart_checkout_outlined)),
                     ),
                   );
                 }).toList(),
@@ -165,7 +165,7 @@ class _SuplemenPageState extends State<SuplemenPage> {
                         MaterialPageRoute(
                             builder: (context) => const CartScreen()));
                   },
-                  child: const Text("Lihat Ke ranjang"),
+                  child: Icon(Icons.shopping_cart_checkout_outlined),
                 ),
               ],
             ),
